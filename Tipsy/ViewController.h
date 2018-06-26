56e376f87cbfcb522088aa90f8ced342aa688699
Amy Liu <maymayliu@fb.com>
2018-06-26 13:19:22 -0700

    Initial Commit

---
+++ Tipsy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipsy
+//
+//  Created by Amy Liu on 6/26/18.
+//  Copyright © 2018 Amy Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
